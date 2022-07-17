2eed3a2211a1e6a513d8ce11d331da22418a52e6
OkazakiRui <okazaki200226@gmail.com>
2022-07-17 23:43:06 +0900

    feat: get post manual

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,28 @@
-use anyhow::{Ok, Result};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
-fn run() -> Result<()> {
-    println!("Hello, world!");
-    Ok(())
+#[get("/hello")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("hello actix!!")
 }
 
-fn main() {
-    if let Err(e) = run() {
-        eprintln!("Error: {}", e);
-        std::process::exit(1);
-    }
+#[post("/echo")]
+async fn echo(body: String) -> impl Responder {
+    HttpResponse::Ok().body(body)
+}
+
+async fn manual_hello() -> impl Responder {
+    HttpResponse::Ok().body("Hey there!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+            .route("/manual", web::get().to(manual_hello))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
