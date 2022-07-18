f3d317099d247827fb43be25a504198b8b274f8f
OkazakiRui <okazaki200226@gmail.com>
2022-07-18 22:32:37 +0900

    feat: todo

--- src/main.rs
+++ src/main.rs
@@ -2,32 +2,57 @@ use std::sync::Mutex;
 
 use actix_web::{web, App, HttpServer};
 
+struct Todo {
+    id: usize,
+    title: String,
+    completed: bool,
+}
+impl Todo {
+    fn new(id: usize, title: String, completed: bool) -> Todo {
+        Todo {
+            id,
+            title,
+            completed,
+        }
+    }
+}
 struct AppState {
-    counter: Mutex<i32>,
+    todo_list: Mutex<Vec<Todo>>,
 }
 async fn index(data: web::Data<AppState>) -> String {
-    let counter = data.counter.lock().expect("lock failed");
+    let todo_list = data.todo_list.lock().expect("lock failed");
 
-    format!("current number: {counter}")
+    todo_list
+        .iter()
+        .map(|todo| {
+            format!(
+                "id: {}\ntitle: {}\ncompleted: {}",
+                todo.id, todo.title, todo.completed
+            )
+        })
+        .collect::<Vec<String>>()
+        .join("\n=========================\n")
 }
-async fn increment(data: web::Data<AppState>) -> String {
-    let mut counter = data.counter.lock().expect("lock failed");
-    *counter += 1;
+// async fn createTask(data: web::Data<AppState>) -> String {
+//     let mut todo_list = data.todo_list.lock().expect("lock failed");
+// *counter += 1;
 
-    format!("increment after number: {counter}")
-}
+// format!("increment after number: {counter}")
+// }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let counter = web::Data::new(AppState {
-        counter: Mutex::new(0),
+    let todo_list = web::Data::new(AppState {
+        todo_list: Mutex::new(vec![
+            Todo::new(1, String::from("shop for groceries"), false),
+            Todo::new(2, String::from("shop for groceries"), false),
+        ]),
     });
 
     HttpServer::new(move || {
         App::new()
-            .app_data(counter.clone())
+            .app_data(todo_list.clone())
             .route("/", web::get().to(index))
-            .route("/increment", web::get().to(increment))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
